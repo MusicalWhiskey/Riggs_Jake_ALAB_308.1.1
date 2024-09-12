a6ebfdfa9d3426bff99e26aebf8cf1e2a798a26e
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-11 20:45:16 -0700

    Part 1

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 // The initial numbers that must be verified.
-const n1 = 13;
-const n2 = 12;
+const n1 = 10;
+const n2 = 15;
 const n3 = 20;
 const n4 = 5;
 
@@ -18,19 +18,19 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
-const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+let isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
 
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
 console.log(isValid);
@@ -43,3 +43,23 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+const isDivisibleBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+  if ((n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) >= 0) {
+    console.log('Divisible By 5');
+  } else {
+    console.log('Not Div By 5');
+  }
+
+  const isLargerThanN4 = (n1 > n4);
+  if (n1 > n4) {
+    console.log('It\'s Bigger');
+  } else {
+    console.log('It\'s not Bigger')
+  }
+
+const firstResult = n2 - n1;
+const secondResult = firstResult * n3;
+const thirdResult = secondResult / n4;
+console.log("The Answer Is", thirdResult);
+
